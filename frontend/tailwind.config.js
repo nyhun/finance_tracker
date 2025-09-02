031cce9f5640a6c9aa870c2e5a8e6aea5a8de751
mathom <nyekics@gmail.com>
2025-09-02 21:22:19 +0200

    setup tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,13 @@
-/** @type {import('tailwindcss').Config} */
+import { materialTailwindConfig } from "@material-tailwind/react/utils";
+
 export default {
-    content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-}
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+    ...materialTailwindConfig.content,
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
